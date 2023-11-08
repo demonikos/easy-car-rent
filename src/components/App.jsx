c5cfb9561efe6f03a5c63471c3101dcebd2ff0c6
demonikos <119507967+demonikos@users.noreply.github.com>
2023-11-09 00:01:46 +0200

    add routes and pages

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,37 @@
+// export const App = () => {
+//   return (
+//     <div
+//       style={{
+//         height: '100vh',
+//         display: 'flex',
+//         justifyContent: 'center',
+//         alignItems: 'center',
+//         fontSize: 40,
+//         color: '#010101'
+//       }}
+//     >
+//       React homework template
+//     </div>
+//   );
+// };
+import { Home } from 'pages/Home';
+import { Catalog } from 'pages/Catalog';
+import { Favorites } from 'pages/Favorites';
+
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <Routes>
+        <Route path="/">
+          <Route index element={<Home />} />
+          <Route path="/catalog" element={<Catalog />} />
+          <Route path="/favorites" element={<Favorites />} />
+        </Route>
+        <Route path="*" element={Home} />
+      </Routes>
+    </>
   );
 };
